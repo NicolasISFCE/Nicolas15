816f2fb6f3e05a789d208b29933528652d9550fd
NP <email@email.com>
2016-12-19 19:00:50 +0100

    Premier commit (en local évidemment)

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Projet GIT ISFCE DvpWeb</h1>
+        <p>Vendredi 19 décembre 2016.</p>
+    </body>
+</html>
