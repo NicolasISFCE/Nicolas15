a79ea39327930ff0d46a91fa1c8466c35ad265e9
fred <fred@LAPTOP-7BMEN45J>
2016-12-19 19:38:46 +0100

    premier commit du collaborateur

--- index.php
+++ index.php
@@ -9,5 +9,7 @@
         <p>Vendredi 19 décembre 2016.</p>
         <h2>Et voici un deuxième titre</h2>
         <h3>Et un troisième</h3>
+        <h4>quatrieme titre</h4>
+        <p>modifications par le collaborateur</p>
     </body>
 </html>
