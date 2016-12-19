21d56cd69baf5d40b4cb9fdfc166bd1705295249
NicolasISFCE <email@email.com>
2016-12-19 20:27:38 +0100

    Ajout d'un modèle de connexion, et pages vides menu/header/footer/présentation

--- index.php
+++ index.php
@@ -1,15 +1,35 @@
+<?php
+session_start();
+
+include("./modeles/connexion_db.php");
+?>
+
 <!DOCTYPE html>
-<html>
+<html lang="fr">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>ISFCE</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="vues/bootstrap-3.3.7-dist/css/bootstrap.min.css">
+        <script src=vues/jquery-3.1.1/jquery-3.1.1.min.js"></script>
+        <script src="vues/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <h1>Projet GIT ISFCE DvpWeb</h1>
-        <p>Vendredi 19 décembre 2016.</p>
-        <h2>Et voici un deuxième titre</h2>
-        <h3>Et un troisième</h3>
-        <h4>quatrieme titre</h4>
-        <p>modifications par le collaborateur</p>
+        <div class="container">
+            <?php
+            include("./vues/menu_header_footer/vue_header.php");
+            include("./vues/menu_header_footer/vue_menu.php");
+
+            $navigation = isset($_GET['navigation']) ? $_GET['navigation'] : 'presentation';
+
+            switch ($navigation) {
+                case 'presentation' :
+                    include("./vues/vue_presentation.php");
+                    break;
+            }
+            include("./vues/menu_header_footer/vue_footer.php");
+            ?>
+
+        </div>
     </body>
 </html>
